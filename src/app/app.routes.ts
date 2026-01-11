d87919c1602b97fdf9511355210fc183d003172b
Divanshu <divanshu.jagtani@xagroup.com>
2026-01-11 23:42:23 +0530

    refactor: remove login and oauth-login components and related styles, tests, and services
    - Deleted LoginComponent and OAuthLoginComponent along with their HTML, SCSS, and spec files.
    - Removed AuthService and AuthInterceptor, including their tests.
    - Cleaned up unused styles and functionality related to the login process.

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,28 +1,10 @@
 import { Routes } from '@angular/router';
 
 export const routes: Routes = [
+
   {
     path: '',
-    loadComponent: () => import('./components/login/login.component').then(m => m.LoginComponent)
-  },
-  {
-    path: 'oauth-login',
-    loadComponent: () => import('./components/oauth-login/oauth-login.component').then(m => m.OAuthLoginComponent)
-  },
-  {
-    path: 'auth0',
     loadComponent: () => import('./components/auth0/auth0.component').then(m => m.Auth0Component)
   },
-  {
-    path: 'google-login',
-    loadComponent: () => import('./components/google-login/google-login.component').then(m => m.GoogleLoginComponent)
-  },
-  {
-    path: 'auth/callback',
-    loadComponent: () => import('./components/google-login/google-login.component').then(m => m.GoogleLoginComponent)
-  },
-  {
-    path: 'dashboard',
-    loadComponent: () => import('./components/dashboard/dashboard.component').then(m => m.DashboardComponent)
-  }
+
 ];

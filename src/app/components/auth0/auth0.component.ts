46368fc2fdeb7fa726cfdacf07fa13355e2282d4
Divanshu <divanshu.jagtani@xagroup.com>
2026-01-16 01:09:15 +0530

    feat: redesign Auth0 component with modern styling and improved user experience

--- src/app/components/auth0/auth0.component.ts
+++ src/app/components/auth0/auth0.component.ts
@@ -1,23 +1,32 @@
-import { AsyncPipe, NgIf } from '@angular/common';
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Observable } from 'rxjs';
 import { LoginButtonComponent } from '../login-button.component';
-import { LogoutButtonComponent } from '../logout-button.component';
 import { AuthApiService } from '../../services/auth-api.service';
-import { ProfileComponent } from '../profile.component';
 import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-auth0',
   standalone: true,
-  imports: [AsyncPipe, LogoutButtonComponent, LoginButtonComponent, NgIf, ProfileComponent],
+  imports: [LoginButtonComponent],
   templateUrl: './auth0.component.html',
   styleUrls: ['./auth0.component.scss']
 })
-export class Auth0Component  {
+export class Auth0Component implements OnInit {
   public isAuthenticated$!: Observable<boolean>;
 
-  constructor(public auth: AuthApiService, private router:Router) {
+  constructor(public auth: AuthApiService, private readonly router: Router) {
     this.isAuthenticated$ = this.auth.isAuthenticated();
+  }
+
+  ngOnInit() {
+    // Add a small delay to allow Auth0 callback to complete
+    setTimeout(() => {
+      this.auth.isAuthenticated().subscribe((isAuth) => {
+        if (isAuth) {
+          console.log('User authenticated, navigating to dashboard');
+          this.router.navigate(['/dashboard']);
+        }
+      });
+    }, 500);
   }
 }

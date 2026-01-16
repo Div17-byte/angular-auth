f0577df882ffeed61c54df9cf0bf296b505a3b06
Divanshuj23 <divanshu.jagtani@westernacher.com>
2026-01-16 09:16:49 +0530

    feat: implement GuestGuard to manage access to login page based on authentication status

--- src/app/components/auth0/auth0.component.ts
+++ src/app/components/auth0/auth0.component.ts
@@ -1,32 +1,14 @@
-import { Component, OnInit } from '@angular/core';
-import { Observable } from 'rxjs';
+import { Component } from '@angular/core';
 import { LoginButtonComponent } from '../login-button.component';
 import { AuthApiService } from '../../services/auth-api.service';
-import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-auth0',
   standalone: true,
   imports: [LoginButtonComponent],
   templateUrl: './auth0.component.html',
-  styleUrls: ['./auth0.component.scss']
+  styleUrls: ['./auth0.component.scss'],
 })
-export class Auth0Component implements OnInit {
-  public isAuthenticated$!: Observable<boolean>;
-
-  constructor(public auth: AuthApiService, private readonly router: Router) {
-    this.isAuthenticated$ = this.auth.isAuthenticated();
-  }
-
-  ngOnInit() {
-    // Add a small delay to allow Auth0 callback to complete
-    setTimeout(() => {
-      this.auth.isAuthenticated().subscribe((isAuth) => {
-        if (isAuth) {
-          console.log('User authenticated, navigating to dashboard');
-          this.router.navigate(['/dashboard']);
-        }
-      });
-    }, 500);
-  }
+export class Auth0Component {
+  constructor(public auth: AuthApiService) {}
 }
